82c28b1b2d5d5fa5ae5aa19271fb44a34bd5c7f2
nilsmart96 <n.marthiensen@gmail.com>
2023-03-16 20:11:15 +0100

    hjkhk

--- edit_dupli_streamlit.py
+++ edit_dupli_streamlit.py
@@ -1,62 +1,18 @@
 import streamlit as st
 import PyPDF2
-import io
 
-# Create a file uploader in Streamlit
+st.title("PDF Reader")
+
+# Allow the user to upload a PDF file
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
-# If the user has uploaded a file
 if uploaded_file is not None:
-    # Open the PDF file in read binary mode using PyPDF2
-    pdf_file = io.BytesIO(uploaded_file.getvalue())
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    # Create a PdfFileReader object
+    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
 
-    # Process the PDF file and create a new PDF writer object
-    pdf_writer = PyPDF2.PdfFileWriter()
-    page_nums = [0]
+    # Extract text from the first page
+    page = pdf_reader.getPage(0)
+    text = page.extractText()
 
-    for i, page in enumerate(pdf_reader.pages):
-        text = page.extractText()
-        st.write(text)
-
-        try:
-            gregory_loc = text.index("Gregory")
-            page_num_1 = text[gregory_loc + 17]
-            page_nums.append(int(page_num_1))
-
-            try:
-                page_num_2 = text[gregory_loc + 18]
-                del page_nums[-1]
-                page_nums.append(int(page_num_1 + page_num_2))
-
-            except IndexError:
-                continue
-
-        except ValueError:
-            page_nums.append(page_nums[-1] + 1)
-
-    del_list = []
-    i = 1
-
-    while i < len(page_nums):
-        if page_nums[i - 1] == page_nums[i]:
-            del_list.append(i - 2)
-
-        i += 1
-
-    for i, page in enumerate(pdf_reader.pages):
-        if i not in del_list:
-            pdf_writer.addPage(page)
-
-    # Write the new PDF file to a bytes buffer
-    pdf_output = io.BytesIO()
-    pdf_writer.write(pdf_output)
-    pdf_output.seek(0)
-
-    # Download the new PDF file using Streamlit's download_button function
-    st.download_button(
-        label="Download updated PDF",
-        data=pdf_output,
-        file_name="updated.pdf",
-        mime="application/pdf"
-    )
+    # Display the extracted text
+    st.text(text)
