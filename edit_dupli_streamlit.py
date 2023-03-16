ae814e58437682cd3e9ef04fb300321cd7731379
nilsmart96 <n.marthiensen@gmail.com>
2023-03-16 19:01:49 +0100

    bugfixing

--- edit_dupli_streamlit.py
+++ edit_dupli_streamlit.py
@@ -14,50 +14,49 @@ def delete_duplicate_pages(input_file_path, output_file_path):
         page_nums = [0]
 
         # Loop through each page of the PDF
-        for i in range(pdf_reader.getNumPages()):
-            # Get the page object
-            page = pdf_reader.getPage(i)
-
+        for i, page in enumerate(pdf_reader.pages):
             # Extract the text from the page
-            text = page.extractText()
-
+            text = page.extract_text()
+        
             try:
                 # Find the index of the text "Gregory"
                 gregory_loc = text.index("Gregory")
-
+        
                 # Get the page number at the bottom right
                 page_num_1 = text[gregory_loc + 17]
                 page_nums.append(int(page_num_1))
-
+        
                 try:
                     # If the page number has two digits
                     page_num_2 = text[gregory_loc + 18]
                     # Replace the last element in page_nums with the two-digit page number
                     del page_nums[-1]
                     page_nums.append(int(page_num_1 + page_num_2))
-
+        
                 except IndexError:
                     # If the page number has one digit, do nothing
                     continue
-
+                
             except ValueError:
                 # If "Gregory" is not found, increment the previous page number by 1
                 page_nums.append(page_nums[-1] + 1)
-
-        # Initialize a set to store the page numbers of duplicate pages
-        duplicate_page_nums = set()
-
-        # Loop through the page numbers and identify duplicate pages
-        for i in range(len(page_nums) - 1):
-            if page_nums[i] == page_nums[i + 1]:
-                duplicate_page_nums.add(i)
-                duplicate_page_nums.add(i + 1)
-
+        
+        # Initialize a list to store the indices of pages to delete
+        del_list = []
+        i = 1
+        while i < len(page_nums):
+            # If two consecutive pages have the same page number
+            if page_nums[i - 1] == page_nums[i]:
+                # Add the index of the previous page to del_list
+                del_list.append(i - 2)
+        
+            i += 1
+        
         # Loop through each page of the PDF again
-        for i in range(pdf_reader.getNumPages()):
-            # If the current page is not a duplicate, add it to the new PDF
-            if i not in duplicate_page_nums:
-                pdf_writer.addPage(pdf_reader.getPage(i))
+        for i, page in enumerate(pdf_reader.pages):
+            # If the current page is not in del_list, add it to the new PDF
+            if i not in del_list:
+                pdf_writer.add_page(page)
 
         # Save the new PDF file in write binary mode
         with open(output_file_path, "wb") as output_file:
